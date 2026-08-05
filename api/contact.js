22257c2c045204712286fa3e8d1392b6f93549fd
Axel <axelsalinas476@gmail.com>
2026-08-05 11:40:36 -0300

    Aceptar solo POST

--- api/contact.js
+++ api/contact.js
@@ -1,3 +1,11 @@
-export default function handler(req, res) {
-    res.status(200).json({ message: "Hola desde la API de axel" });
+export default function handler(req, res){
+    if (req.method !== 'POST') {
+        return res.status(405).json({
+            error: "Metodo no permitido"
+        });
+    }
+
+    return res.status(200).json({
+        mensaje: "La API recibió la solicitud correctamente"
+    });
 }
