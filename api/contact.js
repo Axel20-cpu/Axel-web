2138783dfaba110eeb508ad0bed584856bfa995e
Axel <axelsalinas476@gmail.com>
2026-08-05 12:11:59 -0300

    Prueba de comincacion con la API

--- api/contact.js
+++ api/contact.js
@@ -9,3 +9,15 @@ export default function handler(req, res){
         mensaje: "La API recibió la solicitud correctamente"
     });
 }
+
+//Prueba de comunicación con la API
+
+const testButton = document.querySelector("#test-api");
+testButton.addEventListener("click", async () => {
+    const response = await fetch("/api/contact", {
+        method: "POST",
+    });
+
+    const data = await response.json();
+    console.log(data);
+});
